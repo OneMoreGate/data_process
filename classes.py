f6de6526bb47933d952097ca8e224a58bf1f95d2
OneMoreGate <143797131+OneMoreGate@users.noreply.github.com>
2024-12-27 11:45:55 +0300

    DC_IV_data

--- classes.py
+++ classes.py
@@ -1,4 +1,12 @@
 import os
+import matplotlib
+import matplotlib.pyplot as plt 
+from matplotlib.collections import LineCollection
+import shutil
+import pandas as pd 
+import numpy as np 
+
+
 
 class Folder():
     # инициализация, проверка существования папки, проверка пустоты папки
@@ -101,8 +109,6 @@ class Folder():
                     raise TypeError(f'{i} is not {str} or can\'t be converted to {str}')
             if i in folders_from_dict:
                 existed_folders.append(i)
-            else:
-                raise ValueError(f'subdirectory {i} is not exist in {self.main_folder}')
         for i in existed_folders:
             del self.dict_of_measurments[i]
         if return_dict == True:
@@ -119,3 +125,87 @@ class Folder():
     # абсолютный путь к указанной папке
     def get_abspath(self) -> str:
         return self.main_file_path
+    
+    def get_dict_of_single_folder(self):
+        pass
+
+    def delete_measures_from_folder(self):
+        pass
+    
+
+class Data():
+     
+    def __init__(self, folder: Folder) -> None:
+        if isinstance(folder, Folder):
+            self.folder = folder
+            self.dict_of_measurs = self.folder.dict_of_measurments
+        else:
+            raise TypeError(f'Input must be {Folder} type')
+        
+    def _colored_line(self, voltage, current, c, ax: matplotlib.axes, **lc_kwargs):
+        default_kwargs = {"capstyle": "butt"}
+        default_kwargs.update(lc_kwargs)
+        x = np.asarray(voltage)
+        y = np.asarray(current)
+        x_midpts = np.hstack((x[0], 0.5 * (x[1:] + x[:-1]), x[-1]))
+        y_midpts = np.hstack((y[0], 0.5 * (y[1:] + y[:-1]), y[-1]))
+        coord_start = np.column_stack((x_midpts[:-1], y_midpts[:-1]))[:, np.newaxis, :]
+        coord_mid = np.column_stack((x, y))[:, np.newaxis, :]
+        coord_end = np.column_stack((x_midpts[1:], y_midpts[1:]))[:, np.newaxis, :]
+        segments = np.concatenate((coord_start, coord_mid, coord_end), axis=1)
+        lc = LineCollection(segments, **default_kwargs)
+        lc.set_array(c) 
+        return ax.add_collection(lc)
+        
+    def get_single_DC_IV_data(self, subdirectory: str, file_name: str) -> pd.DataFrame:
+        data_dir = self.folder.get_abspath() + '\\' + subdirectory + '\\' + file_name + '.data'
+        if not os.path.exists(data_dir):
+            raise ValueError(f'path {data_dir} is not exist')
+        dataframe = pd.read_csv(data_dir, delimiter='   ', skiprows=16, engine='python', header=None, encoding='ISO-8859-1').astype(np.float32)
+        dataframe.rename(columns = {0: 'voltage', 1: 'current', 2: 'resistance'}, inplace=True)
+        return dataframe
+    
+    def draw_single_DC_IV_plot(self, voltage: pd.Series, current: pd.Series) -> None:
+        data_len = len(voltage)
+        fig, ax = plt.subplots(figsize = [10,5])
+        ax.set_yscale('log')
+        ax.grid(which='major', linewidth = 0.6)
+        ax.grid(which='minor', linewidth = 0.2)
+        ax.set_xlim(xmin= voltage.min()*1.2, xmax=voltage.max()*1.2)
+        ax.set_ylim(ymin= current.min()*0.2, ymax=current.max()*5)
+        color = np.linspace(0, 1, data_len)
+        lines = self._colored_line(voltage, current, color, ax, cmap = 'plasma')
+        cbar = fig.colorbar(lines)
+        cbar.set_ticks([0, 1])
+        cbar.set_ticklabels(['start','end'], size = 15)
+        
+    def _save_DC_IV_plot(self, subdirectory: str, file_name: str)-> None:
+            if not hasattr(self, 'graphs_path'):
+                local_graph_folder = self.folder.get_abspath()
+            else:
+                local_graph_folder = self.graphs_path + '\\' + subdirectory + '\\' + file_name + '.png'
+            plt.savefig(local_graph_folder, bbox_inches = 'tight', dpi = 200)
+            plt.close()
+
+    def _create_dir(self, path: str) -> None:
+        if os.path.isdir(path):
+            shutil.rmtree(path, ignore_errors=True)
+            os.mkdir(path)
+        else:
+            os.mkdir(path)
+
+    def draw_all_DC_IV(self, graphs_path: str)-> None:
+        self.graphs_path = '\\'.join([os.path.dirname(self.folder.get_abspath()), graphs_path])
+        self._create_dir(self.graphs_path)
+        for folder in list(self.dict_of_measurs.keys()):
+            local_graph_folder = '\\'.join([self.graphs_path, folder])
+            self._create_dir(local_graph_folder)
+            for measur in list(self.dict_of_measurs[folder].keys()):
+                if self.dict_of_measurs[folder][measur] == 'DC IV':
+                    data = self.get_single_DC_IV_data(folder, measur)
+                    voltage = data['voltage']
+                    current = np.abs(data['current'])
+                    self.draw_single_DC_IV_plot(voltage, current)
+                    self._save_DC_IV_plot(folder, measur)
+                else:
+                    continue
